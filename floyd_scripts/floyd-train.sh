1a1396a690316646765ffbacca88063071377311
2ArounD <arnoudjonker@gmail.com>
2020-05-20 20:47:53 +0200

    structure adaptations, created train.py to start trainings of biobert, added the full repository of biobert and created a script to train on floydhub

---
+++ floyd_scripts/floyd-train.sh
@@ -0,0 +1,16 @@
+export OUT_DIR="/output/"
+export NER_DIR="/floyd/input/nerdata/BC2GM/"
+export BIOBERT_DIR="/floyd/input/biobert/biobert_v1.1_pubmed/"
+export NET="squeezeDet+BN"
+
+floyd run \
+--data 2around/datasets/pre_trained_weights/:biobert \
+--data 2around/datasets/nerdata/:nerdata \
+--env tensorflow-1.11 \
+--gpu \
+--max-runtime 1800 \
+ "python3 ./train.py \
+  --biobert_dir=$BIOBERT_DIR \
+  --ner_dir=$NER_DIR \
+  --output_dir=$OUT_DIR"
+
---
+++ train.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+import pdb
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--biobert_dir', dest='biobert_dir' , type=str,
+                    default='../data/pre_training/biobert_v1.1_pubmed/',
+                    help='directory with biobert pretrained weights, config and vocab')
+
+parser.add_argument('--ner_dir', dest='ner_dir' , type=str,
+                    default='../data/NERdata/BC2GM/',
+                    help='directory with named entity reconition dataset')
+
+parser.add_argument('--output_dir', dest='output_dir' , type=str,
+                    default='../out/',
+                    help='directory where outputs are written')
+
+args = parser.parse_args()
+
+#TODO
+#Check if dir end with /
+
+cmd = ('python3 run_ner.py'
+      ' --do_train=true'
+      ' --do_eval=true'
+      ' --vocab_file=' + args.biobert_dir + 'vocab.txt'
+      ' --bert_config_file=' + args.biobert_dir + 'bert_config.json'
+      ' --init_checkpoint=' + args.biobert_dir + 'model.ckpt-1000000'
+      ' --num_train_epochs=60.0'
+      ' --data_dir=' + args.ner_dir +
+      ' --output_dir='  + args.output_dir)
+
+os.chdir('./biobert')
+os.system(cmd)
+
